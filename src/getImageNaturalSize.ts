62046a6cc1d6a636f9634481b383918531e6ebee
wenqing <wenqing@kerrylan.com>
2023-09-19 09:51:21 +0800

    feat: upgrade `getImageNaturalSize` method

--- src/getImageNaturalSize.ts
+++ src/getImageNaturalSize.ts
@@ -10,13 +10,6 @@ const getImageNaturalSize = (imgEl: HTMLImageElement) => {
   if (imgEl.naturalWidth) {
     size.w = imgEl.naturalWidth
     size.h = imgEl.naturalHeight
-  } else {
-    const temp = new Image()
-    const src = imgEl.getAttribute('src') || ''
-
-    temp.src = src
-    size.w = temp.width
-    size.h = temp.height
   }
 
   return size
